e4211838ed8c68e2b3f04c3b6b7c5812abe9bd89
Masahiko Tokita <tokita@pumpup.jp>
2015-05-25 22:27:44 +0900

    pumpcms->PumpkinCMS

--- app/layout/admin/layout.php
+++ app/layout/admin/layout.php
@@ -140,7 +140,7 @@ selector:'.pump_tinymce'
         <!-- header logo: style can be found in header.less -->
         <header class="header">
             <a href="<?php echo PC_Config::url() ?>/admin/" class="logo">
-                pumpcms
+                PumpkinCMS
             </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top" role="navigation">
