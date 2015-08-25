299a98b4f41928f0b02a257b1d5f419e2612f127
Masahiko Tokita <tokita@pumpup.jp>
2015-08-25 13:49:55 +0900

    allow no password

--- app/module/admin/controller/user.php
+++ app/module/admin/controller/user.php
@@ -2,11 +2,15 @@
 
 class admin_user extends PC_Controller {
     public function __construct() {
+	global $pumpform_config; 
 
     	PC_Util::redirect_if_not_site_admin();
     	
-		$this->_flg_scaffold = true;
-		$this->_module = 'user';
-		$this->_table = 'user';
+        $this->_flg_scaffold = true;
+        $this->_module = 'user';
+        $this->_table = 'user';
+	
+	PumpFormConfig::load_config($this->_module, $this->_table);
+	$pumpform_config['user']['user']['column']['password']['required'] = 0;
     }
 }
