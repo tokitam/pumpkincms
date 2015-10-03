0bb55f14b0e2202c5a72a03d6026a686010d182c
Masahiko Tokita <tokita@pumpup.jp>
2015-10-03 17:02:21 +0900

    add text

--- app/module/index/view/index.php
+++ app/module/index/view/index.php
@@ -1,4 +1,15 @@
 		<div class="col-lg-12">
 			<h3>Hello, PumpkinCMS!</h3>
 			<p>Lightweight CMS</p>
+
+			<p>PumpkinCMS is general simple CMS.</p>
+
+			<ul>
+				<li>Multi site manage</li>
+				<li>Multi user</li>
+				<li>Multi language</li>
+				<li>Telephone auth</li>
+				<li>Total image mange/Total file upload manage</li>
+			</ul>
+
 		</div>
