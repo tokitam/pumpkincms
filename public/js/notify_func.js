d995e73a85beaa527a4de195c7929c0324685ac4
Masahiko Tokita <tokita@pumpup.jp>
2015-10-03 18:46:19 +0900

    remove

--- public/js/notify_func.js
+++
@@ -1,5 +0,0 @@
-function pc_notification(message) {
-	$.notify.defaults({ className: "success" });
-	//$.notify(message, { globalPosition:"top center" });
-	$.notify(message, { globalPosition:"top right" });
-}
