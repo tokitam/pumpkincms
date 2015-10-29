4febe4bc705df730ed57fd55954db18e4bfed599
Masahiko Tokita <tokita@pumpup.jp>
2015-10-30 01:05:19 +0900

    Convert to english

--- app/config/default.sample.php
+++ app/config/default.sample.php
@@ -8,24 +8,24 @@ $pc_config['allow_register'] = true;
 $pc_config['default_module'] = 'index';
 $pc_config['default_controller'] = 'index';
 
-// パスワードリセットメールの有効期間 60*5 = 5分
+// Password reset mail expire time 60*5 = 5minitus
 $pc_config['remindpass_time'] = (60 * 5);
 
-// アップロード画像の格納場所
-// PumpUpload::STORE_TYPE_LOCAL : ローカルディスクに格納
-// PumpUpload::STORE_TYPE_DB : データベースへ格納
-// PumpUpload::STORE_TYPE_S3 : AWS S3 へ格納
+// Location of upload image
+// PumpUpload::STORE_TYPE_LOCAL : Local disk
+// PumpUpload::STORE_TYPE_DB : Database
+// PumpUpload::STORE_TYPE_S3 : AWS S3
 $pc_config['pumpimage_store_type'] = PumpUpload::STORE_TYPE_LOCAL;
 
-// アップロードファイルの格納場所
-// PumpUpload::STORE_TYPE_LOCAL : ローカルディスクに格納
-// PumpUpload::STORE_TYPE_DB : データベースへ格納
-// PumpUpload::STORE_TYPE_S3 : AWS S3 へ格納
+// Location of upload file
+// PumpUpload::STORE_TYPE_LOCAL : Local disk
+// PumpUpload::STORE_TYPE_DB : Database
+// PumpUpload::STORE_TYPE_S3 : AWS S3
 $pc_config['pumpfile_store_type'] = PumpUpload::STORE_TYPE_LOCAL;
 
-// アクションログを記録するかの設定
-// true: 記録する
-// true以外： 記録しない
+// Action log setting
+// true: Record
+// true以外： No record
 $pc_config['enable_action_log'] = true;
 
 // log directory
@@ -35,4 +35,5 @@ $pc_config['session.cookie_lifetime'] = (60 * 60 * 24 * 365);
 $pc_config['session.gc_maxlifetime'] = (60 * 60 * 24 * 365);
 
 //$pc_config['password_hash'] = 'MD5';
+//$pc_config['password_hash'] = 'SHA1';
 
