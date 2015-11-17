416719ed2980f2f23f242f834c975396b2d4a157
masahiko tokita <tokita@pumpup.jp>
2015-11-17 10:48:41 +0900

    cleanup

--- app/module/admin/controller/image.php
+++ app/module/admin/controller/image.php
@@ -1,14 +1,10 @@
 <?php
 
-require_once PUMPCMS_APP_PATH . '/module/page/model/page_model.php';
-
 class admin_image extends PC_Controller {
     public function __construct() {
 
 		PC_Util::redirect_if_not_site_admin();
 		
-		PC_Util::include_language_file('page');
-
 		$this->_flg_scaffold = true;
 		$this->_module = 'image';
 		$this->_table = 'image';
