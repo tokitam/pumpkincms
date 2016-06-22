6f8fb52d7c5ed51b08de596438fd41d7a0fe14e2
masahiko tokita <tokita@pumpup.jp>
2016-06-22 10:51:02 +0900

    authentication process

--- app/module/image/controller/image.php
+++ app/module/image/controller/image.php
@@ -2,6 +2,8 @@
 
 class image_image extends PC_Controller {
     public function __construct() {
+		PC_Util::redirect_if_not_site_admin();
+		    
 		$this->_flg_scaffold = true;
 		$this->_module = 'image';
 		$this->_table = 'image';
