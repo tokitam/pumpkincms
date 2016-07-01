97e7a40df9b03418050d7f0035ee031fea4b3a74
tokitam <tokita@pumpup.jp>
2016-07-02 04:00:53 +0900

    user relation

---
+++ app/module/user/controller/switch.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once PUMPCMS_APP_PATH . '/module/user/model/user_model.php';
+require_once PUMPCMS_APP_PATH . '/module/user/class/oauth_util.php';
+
+class user_switch extends PC_Controller {
+
+	public function index() {
+		$switch_user_id = intval($_GET['user_id']);
+		$user_model = new User_Model();
+		$user_model->switch($switch_user_id);
+	}
+}
