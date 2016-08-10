154ec9d30dcac39c06c14052cecb9b7f1abfcec9
masahiko tokita <tokita@pumpup.jp>
2016-08-10 15:43:52 +0900

    cleanup, debug mode off

--- system/pumpmailer.php
+++ system/pumpmailer.php
@@ -6,38 +6,38 @@ require_once PUMPCMS_SYSTEM_PATH . '/util.php';
 class PumpMailer {
     public function __construct() {
     }
-    
+
     public function send($to, $subject, $message) {
-	$mail = new PHPMailer();
+        $mail = new PHPMailer();
 
-	$mail->SMTPDebug = 3;                               // Enable verbose debug output
+        //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->CharSet = 'iso-2022-jp';
-	$mail->Encoding = '7bit';
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->CharSet = 'iso-2022-jp';
+        $mail->Encoding = '7bit';
 
-	$mail->Host = PC_Config::get('phpmailer_host');       // Specify main and backup SMTP servers
-	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = PC_Config::get('phpmailer_user');   // SMTP username
-	$mail->Password = PC_Config::get('phpmailer_pass');   // SMTP password
-	$mail->SMTPSecure = PC_Config::get('phpmailer_secure'); // Enable TLS encryption, ssl also accepted
-	$mail->Port = PC_Config::get('phpmailer_port');       // TCP port to connect to
-	
-	$mail->setFrom(PC_Config::get('from_email'));
-	$mail->addAddress($to);                               // Add a recipient
-	//$mail->addAddress('ellen@example.com');             // Name is optional
-	//$mail->addReplyTo('info@example.com', 'Information');
-	//$mail->addCC('cc@example.com');
-	//$mail->addBCC('bcc@example.com');
-	
-	//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-	//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-	//$mail->isHTML(true);                                  // Set email format to HTML
-	
-	$mail->Subject = $subject;
-	$mail->Body    = $body;
-	//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-	
-	return $mail->send();
+        $mail->Host = PC_Config::get('phpmailer_host');       // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = PC_Config::get('phpmailer_user');   // SMTP username
+        $mail->Password = PC_Config::get('phpmailer_pass');   // SMTP password
+        $mail->SMTPSecure = PC_Config::get('phpmailer_secure'); // Enable TLS encryption, ssl also accepted
+        $mail->Port = PC_Config::get('phpmailer_port');       // TCP port to connect to
+
+        $mail->setFrom(PC_Config::get('from_email'));
+        $mail->addAddress($to);                               // Add a recipient
+        //$mail->addAddress('ellen@example.com');             // Name is optional
+        //$mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        //$mail->isHTML(true);                                  // Set email format to HTML
+
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        return $mail->send();
     }
 }
