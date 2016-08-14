17e85be4b506243e775d6eab0ff4aba5b30d686d
tokitam <tokita@pumpup.jp>
2016-08-14 13:59:37 +0900

    up|

--- app/module/user/language/japanese/main.php
+++ app/module/user/language/japanese/main.php
@@ -49,7 +49,7 @@ define('_MD_USER_SENT_MAIL', '確認メールメールを送信しました！<b
 define('_MD_UESR_SITE_CLOSE', 'メンテナンス中です');
 define('_MD_USER_SITE_CLOSE_INFO', '管理者のみログイン可能です');
 define('_MD_USER_MASTER_ADMIN', 'マスター管理');
-define('_MD_USER_ADD_ACCOUNT', 'アカウント追加');
+define('_MD_USER_ADD_ACCOUNT', 'アカウント連携');
 define('_MD_USER_ADDED_ACCOUNT', 'アカウント追加しました');
 define('_MD_USER_NO_SAME_ACCOUNT', 'ログインユーザと同じアカウントは関連付けできません');
 define('_MD_USER_FOUND_REL_ACCOUNT', '登録済でした');
