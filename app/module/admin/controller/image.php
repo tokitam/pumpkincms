8907a6539ae3971a1d420b874d344516f36f2ecf
Masahiko Tokita <tokita@pumpup.jp>
2017-04-11 01:15:46 +0900

    update

--- app/module/admin/controller/image.php
+++ app/module/admin/controller/image.php
@@ -2,13 +2,19 @@
 
 class admin_image extends PC_Controller {
     public function __construct() {
+        global $pumpform_config; 
 
-		PC_Util::redirect_if_not_site_admin();
+        PC_Util::redirect_if_not_site_admin();
 		
-		$this->_flg_scaffold = true;
-		$this->_module = 'image';
-		$this->_table = 'image';
+        $this->_flg_scaffold = true;
+        $this->_module = 'image';
+        $this->_table = 'image';
+	
+        PumpFormConfig::load_config($this->_module);
+
+        $pumpform_config['image']['image']['column']['width']['editable'] = 0;
+        $pumpform_config['image']['image']['column']['width']['registable'] = 0;
+        $pumpform_config['image']['image']['column']['height']['editable'] = 0;
+        $pumpform_config['image']['image']['column']['height']['registable'] = 0;
     }
 }
-
-
