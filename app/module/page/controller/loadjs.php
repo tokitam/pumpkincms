b14c5c38713e4508804551e0913dd151a01df181
Masahiko Tokita <tokita@pumpup.jp>
2017-05-01 03:36:46 +0900

    loadjs index to page

---
+++ app/module/page/controller/loadjs.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once PUMPCMS_APP_PATH . '/module/page/model/page_model.php';
+
+class page_loadjs extends PC_Controller {
+    public function index() {
+        if (empty($_GET['module']) || empty($_GET['file'])) {
+            exit();
+        }
+
+        header('Content-type: text/javascript; charset: UTF-8');
+
+        $module = $_GET['module'];
+        $file = $_GET['file'];
+        $module = preg_replace('@[^0-9A-Za-z\-.]@', '', $module);
+        $file = preg_replace('@[^0-9A-Za-z\-.]@', '', $file);
+        $path = PUMPCMS_APP_PATH . '/module/' . $module . '/js/' . $file;
+
+        if (is_readable($path)) {
+            readfile($path);
+        }
+        exit();
+    }
+}
