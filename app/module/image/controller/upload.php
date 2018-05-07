7ea37f14418ee0ef3f53abf7998d5abad1c51762
Masahiko Tokita <tokita@pumpup.jp>
2018-05-07 15:37:32 +0900

    upload image from ckeditor4

--- app/module/image/controller/upload.php
+++ app/module/image/controller/upload.php
@@ -1,9 +1,31 @@
 <?php
 
 class image_upload extends PC_Controller {
-    public function __construct() {
-        $this->_flg_scaffold = true;
-        $this->_module = 'image';
-        $this->_table = 'image';
+    public function add() {
+        $column = 'upload';
+        
+        if (empty($_FILES[$column]['tmp_name'])) {
+            echo json_encode(['error' => 1]);
+            exit();
+        }
+
+        $pumpimage = new PumpImage();
+        $image_id = $pumpimage->upload($column);
+
+        if (empty($image_id)) {
+            echo json_encode(['error' => 1]);
+            exit();
+        }
+
+        $json = [];
+        $json['fileName'] = $_FILES[$column]['tmp_name'];
+        if (empty($json['fileName'])) {
+            $json['fileName'] = 'image_file';
+        }
+        $json['uploaded'] = 1;
+        $json['url'] = PumpImage::get_image_url($image_id);
+        header('Content-type: application/json');
+        echo json_encode($json);
+        exit();
     }
 }
